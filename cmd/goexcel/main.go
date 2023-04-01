2cc51cf986a9001ca384f6cd80d84f1f6cd40ce7
k@t99DaDT0 <wate@micous.com>
2023-04-01 11:23:22 +0800

    go excel

---
+++ cmd/goexcel/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dxasu/goexcel"
+)
+
+func main() {
+	err := goexcel.ExcelRun("edinburgh.xlsx", "newyork", func(f *goexcel.ExcelHandle) error {
+		row := f.GetRowNumber()
+		if row == 0 {
+			f.AppendRowCell(1, 2, 3, 4, 5)
+		}
+		f.AppendRowCell(4, 5, 3, 3, 3, 6)
+		fmt.Println(f.GetAllRows())
+		a := f.GetAllRows()
+		b := a[len(a)-1][0]
+		fmt.Println(">>>", b)
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
